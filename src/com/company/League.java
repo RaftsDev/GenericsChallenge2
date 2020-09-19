7749b0643b828cd7f09760a700d9061527542eb0
Ralph F <RaftsDev@gmail.com>
2020-09-19 13:20:30 -0400

    Feat: Change costructor - league field.

--- src/com/company/League.java
+++ src/com/company/League.java
@@ -4,10 +4,9 @@ import java.util.ArrayList;
 
 public class League <T extends Team> {
     private String name;
-    private ArrayList<T> league;
+    private ArrayList<T> league = new ArrayList<T>();
 
     public League(String name) {
         this.name = name;
-        this.league = new ArrayList<T>();
     }
 }
