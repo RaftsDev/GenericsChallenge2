64e4938caf7984e5745c6d1eb0a443378b3700e9
Ralph F <RaftsDev@gmail.com>
2020-09-19 13:25:06 -0400

    Feat: Create Add().

--- src/com/company/League.java
+++ src/com/company/League.java
@@ -9,4 +9,14 @@ public class League <T extends Team> {
     public League(String name) {
         this.name = name;
     }
+
+    public boolean add(T team){
+        if(this.league.contains(team)){
+            System.out.println("League already contain "+team);
+            return false;
+        }else{
+            this.league.add(team);
+            return true;
+        }
+    }
 }
